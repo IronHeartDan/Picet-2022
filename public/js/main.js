6e45c8fc5616cf5bd83dac8a8a4f707b36507f9d
Danish Khan <danishkhan.dk2.dk16@gmail.com>
2021-12-16 08:55:44 +0530

    Init

---
+++ public/js/main.js
@@ -0,0 +1,72 @@
+window.onload = (e) => {
+  let magic = new IntersectionObserver((entries) => {
+    if (entries[0].intersectionRatio > 0) {
+      entries[0].target.style.animation = "PopLeft  ease 250ms 0s";
+      entries[0].target.style.opacity = "1";
+    } else {
+      entries[0].target.style.animation = "none ";
+      entries[0].target.style.opacity = "0";
+    }
+  });
+
+  let animElements = document.querySelectorAll(".anim");
+  animElements.forEach((e) => {
+    magic.observe(e);
+  });
+};
+
+function scrollToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+document.onscroll = (e) => {
+  let scrollToTop = document.getElementById("scrollToTop");
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    scrollToTop.style.opacity = 1;
+    scrollToTop.style.transform = "scale(1)";
+  } else {
+    scrollToTop.style.opacity = 0;
+    scrollToTop.style.transform = "scale(0)";
+  }
+};
+
+
+document.onresize = (e)=>{
+  console.log(e);
+}
+
+
+function toggleMenu(show) {
+  let nav = document.getElementById("nav_menu");
+  let con = document.getElementById("nav_header");
+  let items = document.getElementsByClassName("menu_item");
+  let itemsCon = document.getElementById("nav_itemCon");
+
+  if (show) {
+    document.body.style.overflow = "hidden";
+    con.style.backgroundColor = "#3aafa9";
+    con.style.height = "100vh";
+    con.style.alignItems = "center";
+    itemsCon.style.display = "flex";
+    nav.href = "javascript:toggleMenu(false)";
+    nav.innerHTML = "<img src='./asset/close.svg' alt='menu' />";
+    Array.from(items).forEach((item) => {
+      item.style.display = "block";
+    });
+  } else {
+    document.body.style.overflow = "auto";
+    con.style.alignItems = "unset";
+    itemsCon.style.display = "none";
+    con.style.backgroundColor = "transparent";
+    con.style.height = "60px";
+    nav.href = "javascript:toggleMenu(true)";
+    nav.innerHTML = "<img src='./asset/menu.svg' alt='menu' />";
+    Array.from(items).forEach((item) => {
+      item.style.display = "none";
+    });
+  }
+}
